77b18c81ec456d25d14d38255602c501565b4559
unknown <m.jansen@outlook.fr>
2022-10-19 09:37:42 +0200

    Product.js terminé

--- front/js/product.js
+++ front/js/product.js
@@ -5,39 +5,39 @@ var url = new URL(string);
 let idProduct = url.searchParams.get("id");
 console.log(idProduct);
 
-
+const chosenColor = document.getElementById("colors")
 //Ensuite on récupère les donées de l'Api
 fetch("http://localhost:3000/api/products/" + idProduct)
-    .then((res) => res.json())
-    .then((article) => {
-        console.log(article);
-console.log(article.name)
-console.log(article.description)
-        let name = article.name
-        let description = article.description
-        let price = article.price
-        let nameHtml = document.getElementById("title")
-        nameHtml.textContent = name
-       let priceHtml = document.getElementById("price")
-       priceHtml.textContent = price
+  .then((res) => res.json())
+  .then((article) => {
+    console.log(article);
+    console.log(article.name);
+    console.log(article.description);
+    
+    let name = article.name;
+    let description = article.description;
+    let price = article.price;
 
-    });
+    let nameHtml = document.getElementById("title");
+    nameHtml.textContent = name;
+    let descriptionHtml = document.getElementById("description");
+    descriptionHtml.textContent = description;
+    let priceHtml = document.getElementById("price");
+    priceHtml.textContent = price;
 
+    let productImg = document.createElement("img");
+    document.getElementsByClassName("item__img")[0].appendChild(productImg);
+    productImg.src = article.imageUrl;
+    productImg.alt = article.altTxt;
 
+    for (let color of article.colors){
+      console.log(color)
+      let productColor = document.createElement("option");
+      productColor.value = color;
+      productColor.textContent = color;
+      chosenColor.appendChild(productColor);
+    }
+  });
+  
 
-
-
-// On utilise l'objet url
-//var str = "http://127.0.0.1:5500/front/html/product.html?id=107fb5b75607497b96722bda5b504926";
-//var url = new URL(str);
-var name = url.searchParams.get("description");
-console.log(description)
-// Méthode 'has' pour vérifier si le paramètre existe
-var str = "http://127.0.0.1:5500/front/html/product.html?id=107fb5b75607497b96722bda5b504926";
-var url = new URL(str);
-var search_params = new URLSearchParams(url.search); 
-
-if(search_params.has('price')) {
-  var name = search_params.get('price');
-  console.log(price)
-}
+   
--- front/js/script.js
+++ front/js/script.js
@@ -2,39 +2,38 @@ let objetsproduits =[];
 
 /* Recupération des données de l'API */
 
- fetch("http://localhost:3000/api/products") 
-    // quand on a la réponse on donne le résultat en json.
-    .then((res) => res.json())
-    .then((promise) => {
-      objetsproduits = promise;
-      console.log(objetsproduits);
-   
-      for (let article in objetsproduits) {
-          // Insertion de "l'ancre" 
-          let productLink = document.createElement("a");
-          document.querySelector(".items").appendChild(productLink);//querySelector()  ne renvoie pas une liste des résultats, mais le premier élément qui correspond à la recherche.
-          productLink.href = `product.html?id=${objetsproduits[article]._id}`;
+ fetch("http://localhost:3000/api/products")
+   // quand on a la réponse on donne le résultat en json.
+   .then((res) => res.json())
+   .then((promise) => {
+     objetsproduits = promise;
+     console.log(objetsproduits);
 
-          // Insertion de l'élément "article" 
-          let productArticle = document.createElement("article");
-          productLink.appendChild(productArticle);
+     for (let article in objetsproduits) {
+       // Insertion de "l'ancre"
+       let productLink = document.createElement("a");
+       document.querySelector(".items").appendChild(productLink); //querySelector()  ne renvoie pas une liste des résultats, mais le premier élément qui correspond à la recherche.
+       productLink.href = `product.html?id=${objetsproduits[article]._id}`;
 
-          let productImg = document.createElement ("img");
-          productArticle.appendChild(productImg);
-          productImg.src = objetsproduits[article].imageUrl;
-          productImg.alt = objetsproduits[article].altTxt;
+       // Insertion de l'élément "article"
+       let productArticle = document.createElement("article");
+       productLink.appendChild(productArticle);
 
-          // <h3 class="productName">Kanap name1</h3>
-        let productName = document.createElement("h3");
-        productArticle.appendChild(productName);//appendChild. Cette fonction permet d'ajouter un élément à la liste des enfants du parent depuis lequel la fonction est appelée.
-        productName.classList.add('productName');//ajoute la classe a l'élément 
-        productName.textContent = objetsproduits[article].name;// on modifie le DOM avec .textContent
+       let productImg = document.createElement("img");
+       productArticle.appendChild(productImg);
+       productImg.src = objetsproduits[article].imageUrl;
+       productImg.alt = objetsproduits[article].altTxt;
 
-        // <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-        let productDescription = document.createElement("p");
-        productArticle.appendChild(productDescription);
-        productDescription.classList.add('productDescription');
-        productDescription.textContent = objetsproduits[article].description;
+       // <h3 class="productName">Kanap name1</h3>
+       let productName = document.createElement("h3");
+       productArticle.appendChild(productName); //appendChild. Cette fonction permet d'ajouter un élément à la liste des enfants du parent depuis lequel la fonction est appelée.
+       productName.classList.add("productName"); //ajoute la classe a l'élément
+       productName.textContent = objetsproduits[article].name; // on modifie le DOM avec .textContent
 
-      }
-  });
+       // <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+       let productDescription = document.createElement("p");
+       productArticle.appendChild(productDescription);
+       productDescription.classList.add("productDescription");
+       productDescription.textContent = objetsproduits[article].description;
+     }
+   });
