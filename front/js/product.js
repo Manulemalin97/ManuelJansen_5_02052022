06855ef00762e90046b100901426b40e005bc12a
unknown <m.jansen@outlook.fr>
2022-11-03 13:59:22 +0100

    page product terminé

--- front/js/product.js
+++ front/js/product.js
@@ -1,11 +1,10 @@
-
 //grace a window.location.href on affiche l'id du bon élément sur la page produit
 let string = window.location.href;
 var url = new URL(string);
 let idProduct = url.searchParams.get("id");
 console.log(idProduct);
 
-const chosenColor = document.getElementById("colors")
+const chosenColor = document.getElementById("colors");
 //Ensuite on récupère les donées de l'Api
 fetch("http://localhost:3000/api/products/" + idProduct)
   .then((res) => res.json())
@@ -13,7 +12,7 @@ fetch("http://localhost:3000/api/products/" + idProduct)
     console.log(article);
     console.log(article.name);
     console.log(article.description);
-    
+
     let name = article.name;
     let description = article.description;
     let price = article.price;
@@ -30,29 +29,70 @@ fetch("http://localhost:3000/api/products/" + idProduct)
     productImg.src = article.imageUrl;
     productImg.alt = article.altTxt;
 
-    for (let color of article.colors){
-      console.log(color)
+    for (let color of article.colors) {
+      console.log(color);
       let productColor = document.createElement("option");
       productColor.value = color;
       productColor.textContent = color;
       chosenColor.appendChild(productColor);
+      
     }
-    
-  })
-
-//Début de l'étape 7 : Localstorage
-
-//on écoute le bouton pour enregistrer notre choix dans le localstorage
-let button = document.getElementById("addToCart");
-button.addEventListener("click", ()=>{ 
- 
-//liste des éléments a conserver dans le local storage
-    function storageContent() {
-      localStorage.setItem('color', document.getElementById('colors').value);
-      localStorage.setItem('quantity', document.getElementById('quantity').value);
-      localStorage.setItem('title', document.getElementById('title').value); // exemple mais ne marche pas (undefined)
-      // récupérer l'id car elle est déjà dans le local storage reste la même .. 
-    }
-    storageContent()
+  });
   
-   })
+  // Localstorage
+function createCartArticle(articleId,articleColor,articleQuantity){
+  let cartArticle = {
+    id: articleId, 
+    color: articleColor,
+    quantity: articleQuantity
+  }
+  return cartArticle
+}
+function getCartArticle (articleId, articleColor){
+  let key = "article" + articleId + "_" + articleColor
+  let value = localStorage.getItem(key)
+  let cartArticle = JSON.parse(value)
+  return cartArticle
+}
+function setCartArticle(articleId,articleColor,articleQuantity){
+  let cartArticle = createCartArticle (articleId,articleColor,articleQuantity)
+  let key = "article" + articleId + "_" + articleColor
+let value = JSON.stringify(cartArticle)
+ localStorage.setItem( key,value)
+}
+//on crée notre objet avec nos informations concernant le produit
+
+ setCartArticle("107fb5b75607497b96722bda5b504926", "White", 0)
+
+// //On déclare notre objet dans le localStorage en indiquant son nom de clé, l'id et sa couleur. Ensuite on séréalise et on transforme nos données en chaine de caractère grace a JSON.stringify
+
+// //On modifie nos données du
+ let sinopeWhite = getCartArticle("107fb5b75607497b96722bda5b504926","White")
+ sinopeWhite.quantity++
+setCartArticle (sinopeWhite.id, sinopeWhite.color, sinopeWhite.quantity)
+
+ setCartArticle("107fb5b75607497b96722bda5b504926", "Blue", 0)
+ let sinopeBlue = getCartArticle("107fb5b75607497b96722bda5b504926","Blue")
+ sinopeBlue.quantity += 5
+setCartArticle ( sinopeBlue.id,  sinopeBlue.color,  sinopeBlue.quantity)
+
+
+//le formulaire s'adapte au nombre d'options qu'il y a dans l'objet du produit
+function addToCart(){
+const color = document.getElementById("colors").value
+  const quantity = document.getElementById("quantity").value
+  // on paramètre les propriétés d'utilisateur, si couleur est nulle ou couleur = 0 et si quanitity = null ou 0 on retourne un message derreur 
+  if (color == null || color == "" || quantity == null || quantity == 0){
+   alert("Please select the color and the quantity")
+  }
+  setCartArticle(idProduct,color,quantity)
+}
+ const button = document.getElementById("addToCart")
+ if (button != null){
+ button.addEventListener("click", (e) => {
+   // en locurence ici on écoute notre panier pour savoir quel article doit etre chargé puis récupéré
+  addToCart()
+  window.location.href = "cart.html"
+  })
+  }
+  
