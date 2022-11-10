8b8a3801e6ba3c34daaec0ffbc8147586aab4a6f
unknown <m.jansen@outlook.fr>
2022-11-10 14:23:05 +0100

    Commentaires sur le fonctionnement du procédé de création

--- front/js/cart.js
+++ front/js/cart.js
@@ -15,6 +15,7 @@ function cartArticle() {
     cart.push(itemObject);
   }
 }
+
 // <!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
 //                 <div class="cart__item__img">
 //                   <img src="../images/product01.jpg" alt="Photographie d'un canapé">
@@ -24,7 +25,7 @@ function cartArticle() {
 //                     <h2>Nom du produit</h2>
 //                     <p>Vert</p>
 //                     <p>42,00 €</p>
-//                   </div>   //cart__item__content__description">
+//                   </div>
 //                   <div class="cart__item__content__settings">
 //                     <div class="cart__item__content__settings__quantity">
 //                       <p>Qté : </p>
@@ -33,10 +34,13 @@ function cartArticle() {
 //                     <div class="cart__item__content__settings__delete">
 //                       <p class="deleteItem">Supprimer</p>
 //                     </div>
-//                   </div>   //cart__item__content__settings">
-//                 </div>    //carti item content
+//                   </div>
+//                 </div>
 //               </article> -->
-function displayCartArticle(article) {
+
+
+
+function displayCartArticle(article) {// Notre fonction qui nous sert a afficher nos éléments.
 
   //div de l'image
   let cartItems = document.getElementById("cart__items");// on récupère la section existante qui a pour id: cart__items.
@@ -62,60 +66,60 @@ function displayCartArticle(article) {
   divImgHtml.appendChild(imgHtml); // la valeur revoyée est imgHtml enfant de divImgHtml
 
   //Content
-  let divContentHtml = document.createElement("div"); //on déclare et on crée la div de cart__item__content
+  let divContentHtml = document.createElement("div"); //on déclare le nom de notre variable et on crée la div.
   divContentHtml.className = "cart__item__content"; // on récupère et on attribut la classe cart__item__content à divContentHtml
-  articleHtml.appendChild(divContentHtml);//La valeur renvoyée est divContent (articleHtml enfant de articleHtml)
+  articleHtml.appendChild(divContentHtml);//La valeur renvoyée est divContentHtml (divContentHtml enfant de articleHtml)
 
   //description
-  let divDescription = document.createElement("div");
-  divDescription.className = "cart__item__content__description";
-  divContentHtml.appendChild(divDescription); 
+  let divDescriptionHtml = document.createElement("div");//on déclare le nom de notre variable et on crée l'article.
+  divDescriptionHtml.className = "cart__item__content__description";// on récupère et on attribut la classe cart__item__content à divDescriptionHtml
+  divContentHtml.appendChild(divDescriptionHtml); //La valeur renvoyée est divDescriptionHtml (divDescriptionHtml enfant de divContentHtml)
 
   //title
-  let titleHtml = document.createElement("h2");
-  divDescription.appendChild(titleHtml);
-  titleHtml.textContent = article.name;
+  let titleHtml = document.createElement("h2");//on déclare le nom de notre variable et on crée le titre.
+  titleHtml.textContent = article.name;//textContent récupère le contenu de tous les éléments, y compris <script> et <style>, ce qui n'est pas le cas de innerText.
+  divDescriptionHtml.appendChild(titleHtml);//La valeur renvoyée est titleHtml (titleHtml enfant de divDescriptionHtml)
 
   //color
-  let colorHtml = document.createElement("p");
-  divDescription.appendChild(colorHtml);
-  colorHtml.textContent = article.color;
+  let colorHtml = document.createElement("p");//on déclare le nom de notre variable et on crée la couleur.
+  divDescriptionHtml.appendChild(colorHtml);//La valeur renvoyée est colorHtml (colorHtml enfant de divDescriptionHtml)
+  colorHtml.textContent = article.color;//textContent récupère le contenu de la couleur 
 
   //price
-    let priceHtml =  document.createElement("p");
-    divDescription.appendChild(priceHtml);
-     priceHtml.textContent = article.price;// price non fonctionnel pour le moment .. 
+    let priceHtml =  document.createElement("p");//on déclare le nom de notre variable et on crée le prix.
+     priceHtml.textContent = article.price;// price non fonctionnel pour le moment .. ?/?/?/?/?/?/?/?/?/?/?/?/?/?/?/?/?
+     divDescriptionHtml.appendChild(priceHtml);//La valeur renvoyée est priceHtm (colorHtml enfant de divDescriptionHtml)
 
   //settings
-  let settingsHtml = document.createElement("div");
-  articleHtml.appendChild(settingsHtml);
-  settingsHtml.className = "cart__item__content__settings";
+  let settingsHtml = document.createElement("div");//on déclare le nom de notre variable et on crée la div des réglages.
+  articleHtml.appendChild(settingsHtml);//La valeur renvoyée est settingsHtml (settingsHtml enfant de articleHtml)
+  settingsHtml.className = "cart__item__content__settings";// on récupère et on attribut la classe cart__item__content__settings à settingsHtml
 
-    //quantity content
-    let divQuantityHtml =  document.createElement("div");
-    divQuantityHtml.className = "cart__item__content__settings__quantity";
-    settingsHtml.appendChild(divQuantityHtml);
+    //quantityContent
+    let divQuantityHtml =  document.createElement("div");//on déclare le nom de notre variable et on crée le prix.
+    divQuantityHtml.className = "cart__item__content__settings__quantity";// on récupère et on attribut la classe cart__item__content__settings__quantity à divQuantityHtml
+    settingsHtml.appendChild(divQuantityHtml);//La valeur renvoyée est divQuantityHtml (divQuantityHtml enfant de settingsHtml)
 
     //quantity
-    let quantity_p =  document.createElement("p");
-    divQuantityHtml.appendChild(quantity_p);
-    quantity_p.textContent = article.quantity;
+    let quantity_p =  document.createElement("p");//on déclare le nom de notre variable et on crée l'élément p.
+    quantity_p.textContent = article.quantity;//textContent récupère le contenu de la quantité
+    divQuantityHtml.appendChild(quantity_p);//La valeur renvoyée est quantity_p (settingsHtml enfant de  divQuantityHtml)
 
     //input
-    let inputHtml = document.createElement("input");
-    inputHtml.className =  "itemQuantity"; // La classe de input est itemQuantity
+    let inputHtml = document.createElement("input");//on déclare le nom de notre variable et on crée l'élément input.
+    inputHtml.className =  "itemQuantity"; // La classe de input est itemQuantity///////////Que faire avec l'input ?/?/?/?/?/?/?/?/?
     divQuantityHtml.appendChild(inputHtml);// Comme quantity, inputHtml est enfant de divQuantityHtml
 
     //settingsDelete
-    let settingsDeleteHtml = document.createElement("div");
-settingsDeleteHtml.className = "cart__item__content__settings__delete";
-settingsHtml.appendChild(settingsDeleteHtml)
+    let settingsDeleteHtml = document.createElement("div");//on déclare le nom de notre variable et on crée l'élément div.
+settingsDeleteHtml.className = "cart__item__content__settings__delete";// La classe de settingsDeleteHtml est cart__item__content__settings__delete
+settingsHtml.appendChild(settingsDeleteHtml);//La valeur renvoyée est settingsDeleteHtml (settingsDeleteHtml enfant de  settingsHtml)
 
 //<p class="deleteItem">Supprimer</p>
 //Delete item
-let deleteItemHtml = document.createElement("p");
-deleteItemHtml.className = "deleteItem";
-settingsDeleteHtml.appendChild(deleteItemHtml)
+let deleteItemHtml = document.createElement("p");//on déclare le nom de notre variable et on crée l'élément p.
+deleteItemHtml.className = "deleteItem";//La classe de deleteItemHtml est deleteItem
+settingsDeleteHtml.appendChild(deleteItemHtml);//La valeur renvoyée est deleteItemHtml (deleteItemHtml enfant de  settingsDeleteHtml)
 }
 
 //La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau
